b78766320956fa55bd65c0b62d9680232681ede8
mawaeg <62243375+mawaeg@users.noreply.github.com>
2026-01-18 13:24:34 +0100

    Disconnect double websocket connections
    - An user shall only be able to connect once to properly handle events
    - Also now a Hello World message is be sent during connection

--- api/routers/websocket.py
+++ api/routers/websocket.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect, WebSocketException, status
 
 from api.models.database_models import DBUser
 from api.utils.security import get_current_user_ws
@@ -15,8 +15,12 @@ async def websocket_endpoint(
     current_user: Annotated[DBUser, Depends(get_current_user_ws)],
     ws_handler: Annotated[WebsocketHandler, Depends(get_websocket_handler)],
 ):
+    if not ws_handler.add(current_user, websocket):
+        return await websocket.close(1008)
+
     await websocket.accept()
-    ws_handler.add(current_user, websocket)
+    await websocket.send_json({"message": "Hello World!"})
+
     print(f"Accepted connection with {current_user.username}")
     try:
         while True:
